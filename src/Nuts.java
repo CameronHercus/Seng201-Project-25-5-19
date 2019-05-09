9e57b39a5f1d9bf3b90e8d3326774c24de201d71
Cherub2K <48897247>
2019-05-09 15:43:39 +1200

    Update Nuts.java

--- src/Nuts.java
+++ src/Nuts.java
@@ -1,7 +1,7 @@
 
-public class Nuts {
+public class Nuts extends FoodItems{
     public Nuts() {
-		
+    	super("Nuts", 10, 10);
 	}
 	public void eatNuts(CrewMembersMainClass tempCrewMember) {
 		//heals a cetain amount of hunger
