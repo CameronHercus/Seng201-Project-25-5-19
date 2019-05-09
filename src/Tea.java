ccd4415383019a19d81f848221fd57544b43da18
Cherub2K <48897247>
2019-05-09 15:43:43 +1200

    Update Tea.java

--- src/Tea.java
+++ src/Tea.java
@@ -1,7 +1,7 @@
 
 public class Tea extends FoodItems {
     public Tea() {
-		
+    	super("Tea", 5, 5);
 	}
 	public void eatTea(CrewMembersMainClass tempCrewMember) {
 		// heals a certain amount of hunger
