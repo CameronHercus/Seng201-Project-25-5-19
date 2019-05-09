365554727dbc5caf7d73be577b30d14c93e24fd8
Cherub2K <48897247>
2019-05-09 15:43:22 +1200

    Create Apple.java

---
+++ src/Apple.java
@@ -0,0 +1,10 @@
+
+public class Apple extends FoodItems {
+	public Apple() {
+		super("Apple", 15, 15);
+	}
+	public void eatApple(CrewMembersMainClass tempCrewMember) {
+		// do you know gods of death love apples
+	}
+
+}
