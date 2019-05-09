b6f4e12a507ff2ec3cf32a00a96e483ae720185a
Cherub2K <48897247>
2019-05-09 15:43:29 +1200

    Create CornedBeef.java

---
+++ src/CornedBeef.java
@@ -0,0 +1,10 @@
+
+public class CornedBeef extends FoodItems {
+	public CornedBeef () {
+		super("CornedBeef", 30, 30);
+	}
+	public void eatCornedBeef(CrewMembersMainClass tempCrewMember) {
+		// heals a certain amount of huner
+	}
+
+}
