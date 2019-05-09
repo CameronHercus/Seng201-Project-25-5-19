5fe1f39753230917c68700dfb5ff015aebf12690
Cherub2K <48897247>
2019-05-09 15:43:27 +1200

    Update CornBeef.java

--- src/CornBeef.java
+++ src/CornBeef.java
@@ -1,7 +1,7 @@
 
-public class CornBeef extends FoodItems {
+public class CornBeef extends FoodItems{
     public CornBeef() {
-		
+    	super("CornBeef", 30, 30);
 	}
 	public void eatCornBeef(CrewMembersMainClass tempCrewMember) {
 		// heals a certain amount of huner
