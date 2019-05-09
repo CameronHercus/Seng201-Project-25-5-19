06656f54fe442b2adf346a37a93e093b841b13f1
Cherub2K <48897247>
2019-05-09 15:43:41 +1200

    Update Soup.java

--- src/Soup.java
+++ src/Soup.java
@@ -1,7 +1,7 @@
 
 public class Soup extends FoodItems {
     public Soup() {
-		
+    	super("Soup", 25, 25);
 	}
 	public void eatSoup(CrewMembersMainClass tempCrewMember) {
 		//heals a certain amount of hunger
