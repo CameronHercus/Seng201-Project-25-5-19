89d3a1a4e2a508c60c1a964ed35cc8408130df94
Cherub2K <48897247>
2019-05-09 15:43:25 +1200

    Update Bread.java

--- src/Bread.java
+++ src/Bread.java
@@ -1,7 +1,7 @@
 
 public class Bread extends FoodItems {
     public Bread() {
-		
+    	super("Bread", 20, 20);
 	}
 	public void eatBread(CrewMembersMainClass tempCrewMember) {
 		//heals a certain amount of hunger
