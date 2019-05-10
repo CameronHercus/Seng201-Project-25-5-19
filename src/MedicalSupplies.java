2168d4f06a90d120146c37c24814bf7a3c845514
Cherub2K <48897247>
2019-05-10 12:28:52 +1200

    Update MedicalSupplies.java

--- src/MedicalSupplies.java
+++ src/MedicalSupplies.java
@@ -1,7 +1,6 @@
 
 public class MedicalSupplies {
     private String medicineName;
-	private int medicineQuantity;
 	private int medicineCost;
 	public MedicalSupplies(String tempMedicineName, int tempMedicineCost) {
 		medicineName = tempMedicineName;
@@ -12,22 +11,8 @@ public class MedicalSupplies {
 	public String getMedicineName() {
 		return medicineName;
 	}
-	public void setMedicineQuantity(int tempQuantity) {
-		medicineQuantity = tempQuantity;
-		// may need to be changed
-	}
-	public int getMedicineQuantity() {
-		return medicineQuantity;
-	}
 	public int getMedicineCost() {
 		return medicineCost;
-	}
-	public boolean getMedicineAvailability() {
-		if (medicineQuantity > 0) {
-			return true;
-		}
-		return false;
-		// needs to be tested but should work
 	}
 	public String toString() {
 		return medicineName;
