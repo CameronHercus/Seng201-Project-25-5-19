a590df4fe208fcdced5ac543a50368eaef73b09e
Cherub2K <48897247>
2019-05-10 12:29:06 +1200

    Update CrewMembersMainClass.java

--- src/CrewMembersMainClass.java
+++ src/CrewMembersMainClass.java
@@ -8,21 +8,14 @@ public class CrewMembersMainClass {
 	private String memberName;
 	private boolean spacePlagueStatus = false;
 	private int crewActions = 2;
-	
-	
-	// not sure if this is a good idea
-
-	
-	//This is the main class for the crewmembers which extends to the individual subclasses such as haggler and medic!
-
 	public CrewMembersMainClass(String tempName, int tempHealthLevel, int tempHungerLevel, int tempTirednessLevel,
 			String tempSkill, String tempMemberName) {
 		memberName = tempMemberName;
-		className = tempName;
 		healthLevel = tempHealthLevel;
 		hungerLevel = tempHungerLevel;
 		tirednessLevel = tempTirednessLevel;
 		skill = tempSkill;
+		className = tempName;
 	}
 	public void setMemberName(String tempMemberName) {
 		memberName = tempMemberName;
@@ -33,43 +26,31 @@ public class CrewMembersMainClass {
 	public String getClassName() {
 		return className;
 	}
-	
 	public void setCrewActions(int newamount) {
 		crewActions = newamount;
 	}
 	public int getCrewActions() {
 		return crewActions;
 	}
-	
-	
-	
 	public void setSpacePlagueStatus(boolean tempStatus) {
 		spacePlagueStatus = tempStatus;
-		// not sure if this should be implimented under the crew member class or ship class
+		// not sure if this should be implemented under the crew member class or ship class
 	}
 	public boolean getSpacePlagueStatus() {
 		return spacePlagueStatus;
 	}
-	
-	
 	public void setHealthLevel(int tempHealthLevel) {
 		healthLevel = tempHealthLevel;
 	}
 	public int getHealthLevel() {
 		return healthLevel;
 	}
-	
-	
-	
 	public void setHungerLevel(int tempHungerLevel) {
 		hungerLevel = tempHungerLevel;
 	}
 	public int getHungerLevel() {
 		return hungerLevel;
 	}
-	
-	
-	
 	public int getTirednessLevel() {
 		return tirednessLevel;
 	}
